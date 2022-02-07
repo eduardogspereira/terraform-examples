4a04b6fadd4d029f97b3a3d15842c4d1ed42f450
Eduardo G. S. Pereira <edu_vcd@hotmail.com>
2022-02-07 22:44:56 +0000

    feat: add demo of terraform cloud module usage

--- 1_terraform/8_terraform_cloud_module/main.tf
+++ 1_terraform/8_terraform_cloud_module/main.tf
@@ -2,6 +2,13 @@ module "module-test" {
   source  = "app.terraform.io/eduardogspereira/module-test/registry"
   version = "0.0.2"
 }
+/*
+This would be the import if the module was public
+*/
+#module "module-test" {
+#  source  = "eduardogspereira/module-test/registry"
+#  version = "0.0.2"
+#}
 
 output "values" {
   value = module.module-test.test_random_uuid_values
