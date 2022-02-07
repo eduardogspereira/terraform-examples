d6c1121b1a36b219754fadd7aa991118dbb09bae
Eduardo G. S. Pereira <edu_vcd@hotmail.com>
2022-02-07 08:22:53 +0000

    chore: improve sample config example

--- 1_terraform/0_sample_config/main.tf
+++ 1_terraform/0_sample_config/main.tf
@@ -11,13 +11,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+locals {
+  common_tags = {
+    Owner = "TeamA"
+  }
+}
+
 resource "aws_s3_bucket" "hello_world" {
   bucket = "hello-world-asdfeaafbcafdeasa"
   acl    = "private"
 
-  tags = {
-    Owner = "TeamA"
-  }
+  tags = merge({
+    BucketRandomData = "random-data",
+  }, local.common_tags)
 }
 
 output "hello_world_arn" {
