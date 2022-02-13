63f186c8f63a4aee2dc6edb1d7c80c410d445cab
Eduardo G. S. Pereira <edu_vcd@hotmail.com>
2022-02-13 23:26:07 +0000

    chore: add more stacks

--- 1_terraform/11_terraform_stacks/0_terraform_workspaces/stacks.tf
+++ 1_terraform/11_terraform_stacks/0_terraform_workspaces/stacks.tf
@@ -6,3 +6,30 @@ module "pandero" {
   aws_access_key_id     = var.aws_access_key_id
   aws_secret_access_key = var.aws_secret_access_key
 }
+
+module "violao" {
+  source                = "./modules/tfe_stack"
+  stack_name            = "violao"
+  organisation_id       = tfe_organization.applications.id
+  oauth_token_id        = tfe_oauth_client.oauth_eduardogspereira.oauth_token_id
+  aws_access_key_id     = var.aws_access_key_id
+  aws_secret_access_key = var.aws_secret_access_key
+}
+
+module "gaita" {
+  source                = "./modules/tfe_stack"
+  stack_name            = "gaita"
+  organisation_id       = tfe_organization.applications.id
+  oauth_token_id        = tfe_oauth_client.oauth_eduardogspereira.oauth_token_id
+  aws_access_key_id     = var.aws_access_key_id
+  aws_secret_access_key = var.aws_secret_access_key
+}
+
+module "cavaquinho" {
+  source                = "./modules/tfe_stack"
+  stack_name            = "cavaquinho"
+  organisation_id       = tfe_organization.applications.id
+  oauth_token_id        = tfe_oauth_client.oauth_eduardogspereira.oauth_token_id
+  aws_access_key_id     = var.aws_access_key_id
+  aws_secret_access_key = var.aws_secret_access_key
+}
