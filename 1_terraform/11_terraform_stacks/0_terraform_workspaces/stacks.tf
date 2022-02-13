cb2cb429e014a60311ec7eeb180a3659004fa827
Eduardo G. S. Pereira <edu_vcd@hotmail.com>
2022-02-13 23:23:30 +0000

    chore: add aws environments

--- 1_terraform/11_terraform_stacks/0_terraform_workspaces/stacks.tf
+++ 1_terraform/11_terraform_stacks/0_terraform_workspaces/stacks.tf
@@ -1,7 +1,8 @@
 module "pandero" {
-  source              = "./modules/tfe_stack"
-  stack_name          = "pandero"
-  organisation_id     = tfe_organization.applications.id
-  environment_secrets = var.environment_secrets
-  oauth_token_id      = tfe_oauth_client.oauth_eduardogspereira.oauth_token_id
+  source                = "./modules/tfe_stack"
+  stack_name            = "pandero"
+  organisation_id       = tfe_organization.applications.id
+  oauth_token_id        = tfe_oauth_client.oauth_eduardogspereira.oauth_token_id
+  aws_access_key_id     = var.aws_access_key_id
+  aws_secret_access_key = var.aws_secret_access_key
 }
